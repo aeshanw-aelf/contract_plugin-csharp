59a8e3adca1d49c82c55c7a1ad11805e451f5be3
Aeshan Wijetunge <aeshan.w@aelf.io>
2023-09-21 15:32:30 +0800

    rename files to match Class-names. Interface to handle 1 & 2 types

--- ContractPlugin/ContractEventClassGenerator.cs
+++ ContractPlugin/ContractEventClassGenerator.cs
@@ -2,7 +2,7 @@ using Google.Protobuf.Reflection;
 
 namespace ContractPluginCsharp;
 
-public class ContractEventClassGenerator
+public class ContractEventClassGenerator: IGenerator<MessageDescriptor,uint>
 {
     //TODO Implement following https://github.com/AElfProject/contract-plugin/blob/453bebfec0dd2fdcc06d86037055c80721d24e8a/src/contract_csharp_generator.cc#L546
     public string Generate(MessageDescriptor descriptor, uint flags)
--- ContractPlugin/IGenerator.cs
+++ ContractPlugin/IGenerator.cs
@@ -4,3 +4,8 @@ public interface IGenerator<in T,V>
 {
     public string Generate(T descriptor,V flags);
 }
+
+public interface IGenerator<in T>
+{
+    public string Generate(T descriptor);
+}
